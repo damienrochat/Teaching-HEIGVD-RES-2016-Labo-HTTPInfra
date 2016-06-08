5d28204cd6d50d69923f353227a789a2af047878
Damien Rochat <damien.rochat@heig-vd.ch>
2016-06-08 09:56:24 +0000

    Install Serf

--- commands.sh
+++ commands.sh
@@ -5,17 +5,8 @@ docker rmi res/apache
 docker rmi res/express
 docker rmi res/reverse-proxy
 
-docker build --tag res/apache --file ./docker/apache/Dockerfile ./docker/apache/
-docker build --tag res/express --file ./docker/express/Dockerfile ./docker/express/
+#docker build --tag res/apache --file ./docker/apache/Dockerfile ./docker/apache/
+#docker build --tag res/express --file ./docker/express/Dockerfile ./docker/express/
 docker build --tag res/reverse-proxy --file ./docker/reverse-proxy/Dockerfile ./docker/reverse-proxy/
 
-docker run -d --name res_apache_static_1 res/apache
-docker run -d --name res_apache_static_2 res/apache
-docker run -d --name res_express_dynamic_1 res/express
-docker run -d --name res_express_dynamic_2 res/express
-docker run \
-	-e STATIC_APP_1=172.17.0.2 \
-	-e STATIC_APP_2=172.17.0.3 \
-	-e DYNAMIC_APP_1=172.17.0.4:3000 \
-	-e DYNAMIC_APP_2=172.17.0.5:3000 \
-	-p 8080:80 --name res_reverse_proxy res/reverse-proxy
+docker run --name res_reverse_proxy res/reverse-proxy
--- docker/reverse-proxy/scripts/setup.sh
+++ docker/reverse-proxy/scripts/setup.sh
@@ -3,4 +3,6 @@
 php /etc/apache2/templates/reverse-proxy.php > /etc/apache2/sites-available/001-reverse-proxy.conf
 a2ensite 001-reverse-proxy.conf
 
+serf agent &
+
 apache2-foreground
